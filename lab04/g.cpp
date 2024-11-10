a513930f57161ba74b9f41104339e45ba965aae7
aksa001 <akzholsadyk12@gmail.com>
2024-11-11 01:12:14 +0600

    lab09

--- lab04/g.cpp
+++ lab04/g.cpp
@@ -1,38 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int count1543(const vector<string>& layer) {
-    string target = "1543";
-    int count = 0;
-    string s = "";
-    for (const auto& row : layer) {
-        s += row;
-    }
-    for (size_t i = 0; i <= s.size() - 4; ++i) {
-        if (s.substr(i, 4) == target) {
-            count++;
-        }
-    }
-    return count;
-}
+bool ako(const vector<int>& nums, int target) {
+    unordered_set<int> current, next;
+    current.insert(nums[0]);
 
-vector<string> extractLayer(const vector<string>& carpet, int layer) {
-    int n = carpet.size();
-    int m = carpet[0].size();
-    vector<string> result;
-    for (int i = layer; i < m - layer; ++i) {
-        result.push_back(string(1, carpet[layer][i]));
+    for (int i = 1; i < nums.size(); ++i) {
+        next.clear();
+        for (int val : current) {
+            next.insert(val + nums[i]);
+            next.insert(val * nums[i]);
+        }
+        current.swap(next);
     }
-    for (int i = layer + 1; i < n - layer; ++i) {
-        result.push_back(string(1, carpet[i][m - layer - 1]));
-    }
-    for (int i = m - layer - 2; i >= layer; --i) {
-        result.push_back(string(1, carpet[n - layer - 1][i]));
-    }
-    for (int i = n - layer - 2; i > layer; --i) {
-        result.push_back(string(1, carpet[i][layer]));
-    }
-    return result;
+
+    return current.count(target) > 0;
 }
 
 int main() {
@@ -41,18 +23,15 @@ int main() {
     while (t--) {
         int n, m;
         cin >> n >> m;
-        vector<string> carpet(n);
+        vector<int> nums(n);
         for (int i = 0; i < n; ++i) {
-            cin >> carpet[i];
+            cin >> nums[i];
         }
-        
-        int totalCount = 0;
-        for (int layer = 0; layer < min(n, m) / 2; ++layer) {
-            vector<string> currentLayer = extractLayer(carpet, layer);
-            totalCount += count1543(currentLayer);
+        if (ako(nums, m)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
         }
-        
-        cout << totalCount << endl;
     }
     return 0;
 }
--- lab09/a.cpp
+++ lab09/a.cpp
@@ -1,75 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector <int> prefix;
 
-void computeLPSArray(string pattern, int M, vector<int>& lps) {
-    int length = 0;
-    lps[0] = 0;
-    int i = 1;
-    while (i < M) {
-        if (pattern[i] == pattern[length]) {
-            length++;
-            lps[i] = length;
-            i++;
-        } else {
-            if (length != 0) {
-                length = lps[length - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
-        }
+vector <int> getPref(string s){
+    prefix.resize(s.size());
+    for(int i = 1; i < s.size(); i++){
+        int j = prefix[i-1];
+        while(j > 0 && s[i] != s[j]) j = prefix[j-1];
+        if(s[i] == s[j]) j++;
+        prefix[i] = j;
     }
+    return prefix;
 }
 
-void KMPSearch(string pattern, string text) {
-    int M = pattern.length();
-    int N = text.length();
-    vector<int> lps(M);
-    int count = 0;
-    deque<int> ako;
-    computeLPSArray(pattern, M, lps);
-
-    int i = 0; 
-    int j = 0; 
-    while (i < N) {
-        if (pattern[j] == text[i]) {
-            i++;
-            j++;
-        }
-
-        if (j == M) {
-            ako.push_back(i - j+1);
-            count++;
-            j = lps[j - 1];
-        } else if (i < N && pattern[j] != text[i]) {
-            if (j != 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
+bool SOL_KMP_GOI(string s, string t){
+    string join = t + '$' + s;
+    vector <int> res = getPref(join);
+    for(int i = t.size() + 1; i < join.size(); i++){
+        if(res[i] == t.size()){
+            return true;
         }
     }
-    cout << count << endl;
-    for(int i=0;i<ako.size();i++){
-        cout << ako[i] << " ";
-    }
+    return false;
 }
 
-int main() {
-    string a,b;cin >> a >> b;
-    string text;
-    string pattern;
-    if(a.size()>b.size()){
-        text = a;
-        pattern = b;
-    }
-    else{
-        text = b;
-        pattern = a;
-    }
-    
 
-    KMPSearch(pattern, text);
-    return 0;
+int main(){
+    string s, t, addedstr; int counter = 1;
+    cin >> s >> t;
+    addedstr = s;
+    while(addedstr.size() < t.size()){addedstr += s; counter++;}
+    if(SOL_KMP_GOI(addedstr, t)) cout << counter;
+    else if(SOL_KMP_GOI(addedstr + s, t)) cout << counter + 1;
+    else cout << -1;
 }
