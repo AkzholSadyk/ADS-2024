ddc381fe4ad2d0281a14fe15be180c1c01a015c3
aksa001 <akzholsadyk12@gmail.com>
2024-12-18 01:36:41 +0600

    aa

--- lab04/g.cpp
+++ lab04/g.cpp
@@ -1,59 +1,23 @@
 #include <bits/stdc++.h>
+// aksa001
 using namespace std;
-
-int main() {
-    long long t;
-    cin >> t;
-
-    while (t--) {
-        long long n;
-        cin >> n;
-        vector<long long> a(n);
-        map<long long, long long> freq;
-
-        for (long long i = 0; i < n; i++) {
-            cin >> a[i];
-            freq[a[i]]++;
-        }
-
-        vector<long long> candidates;
-        for (const auto& pair : freq) {
-            if (pair.second >= 2) {
-                candidates.push_back(pair.first);
+ 
+int main(){
+    int t;cin >> t;
+    vector<int> ako(t);
+    for(int i=0;i<t;i++){
+        cin>>ako[i];
+    }
+    for(int i=0;i<t;i++){
+        int n=ako[i];
+        int m=0;
+        for(int b=1;b<n;b++){
+            int a=n-b;
+            if(a>0){
+                m++;
             }
         }
-
-        if (candidates.size() < 2) {
-            cout << "NO" << endl;
-            continue;
-        }
-
-        sort(candidates.begin(), candidates.end());
-        long long max_area = 0;
-        vector<long long> best_coords(8);
-
-        for (size_t i = 0; i < candidates.size(); i++) {
-            for (size_t j = i + 1; j < candidates.size(); j++) {
-                long long x1 = candidates[i];
-                long long x2 = candidates[j];
-                long long area = (x2 - x1) * (x2 - x1);
-                if (area > max_area) {
-                    max_area = area;
-                    best_coords = {x1, x1, x2, x1, x1, x2, x2, x2};
-                }
-            }
-        }
-
-        if (max_area == 0) {
-            cout << "NO" << endl;
-        } else {
-            cout << "YES" << endl;
-            for (long long coord : best_coords) {
-                cout << coord << " ";
-            }
-            cout << endl;
-        }
+        cout<<m<<endl;
     }
 
-    return 0;
 }
