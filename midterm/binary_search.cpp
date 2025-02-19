9f6c7a21d8ae4a0c74b4571f05ea8ea5d35515a5
aksa001 <akzholsadyk12@gmail.com>
2025-02-19 23:06:46 +0500

    aa

--- midterm/binary_search.cpp
+++ midterm/binary_search.cpp
@@ -1,35 +1,31 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int binary_search(int x, vector<int>& v){
-    int l = 0, r = v.size()-1;
-    while(l<=r){
-        int mid = l+ (r-l)/2;
-        if(v[mid] == x){
-            return mid;
-        }
-        else if(v[mid]<x){
-            l = mid+1;
-        } 
-        else r = mid -1;
+void ako(int a) { 
+    int b;cin >> b;
+    deque<int> ako;
+    for (int i = 0; i < a; i++) {
+        int e;cin >> e; 
+        ako.push_back(e);    
     }
-    return l;
+    bool era = false;
+    for(int i=0;i<ako.size();i++){
+        if(ako[i]==1 and ako[i+1]==1){
+            era = true;
+        }
+        else{
+            era = false;
+        }
+    }
 }
 
-int main() 
-{ 
-    int n, m; cin >> n >> m;
-    vector<int> v(n);
-    int sum = 0;
-    for(int i = 0; i < n; i++){
-        int x;cin >> x;
-        sum+=x;
-        v[i] = sum;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ako(n);
     }
-    while(m--){
-        int t; cin >> t;
-        cout << binary_search(t, v)+1 << endl;
-    }
-
+    return 0;
 }
